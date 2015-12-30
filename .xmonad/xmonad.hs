d074f1f39ae5ec7d138a9fcf302c53fdf46be7bc
Daniel Asher Resnick <danwizard208@gmail.com>
2015-12-30 01:22:53 -0600

    Add xmonad keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,7 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import XMonad.StackSet
 import XMonad.Util.Scratchpad
 import System.IO
@@ -12,40 +12,35 @@ mainModMask = mod4Mask
 myLauncher = "$(yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*' -nb '#000000' -nf '#FFFFFF' -sb '#7C7C7C' -sf '#CEFFAC')"
 
 main = do
-      xmobarProc <- spawnPipe "/usr/bin/xmobar $HOME/.xmobarrc"
-      xmonad $ (myConfig xmobarProc) `additionalKeys` 
-        [ 
-      ----  Power-state keys
-        --  ((mainModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        --, ((mainModMask .|. shiftMask, xK_w), 
-        --    spawn "gksudo -g --message 'Suspend?' pm-suspend")
-        --, ((mainModMask .|. shiftMask, xK_e), 
-        --    spawn "gksudo -g --message 'Shut down?' poweroff")
-        --, ((mainModMask .|. shiftMask, xK_r), 
-        --    spawn "gksudo -g --message 'Reboot?' reboot")
-        --, ((mainModMask .|. shiftMask, xK_y), 
-        --    spawn "gksudo -g --message 'Hibernate?' pm-hibernate")
-      ----  Utility keys
-          ((mainModMask, xK_d), spawn "gmrun")
-        , ((mainModMask, xK_F12), scratchpadSpawnActionTerminal "gnome-terminal")
-        , ((mainModMask, xK_p), spawn myLauncher)
-        --, ((controlMask, xK_Print), 
-        --    spawn "sleep 0.2; scrot -s -e \"mv \\$f ${SCREENSHOTS_DIR}\"")
-        --, ((0, xK_Print), spawn "scrot -e \"mv \\$f ${SCREENSHOTS_DIR}\"")
-      ----  Banshee keys
-        --, ((controlMask .|. shiftMask, xK_Right),  
-        --    spawn "banshee --next")
-        --, ((controlMask .|. shiftMask, xK_Left),   
-        --    spawn "banshee --restart-or-previous")
-        --            "notify-send Banshee\\ `banshee --query-volume`")
-        --, ((controlMask .|. shiftMask, xK_Down),   
-        --    spawn $ "banshee --set-volume=-10;"++
-        --            "notify-send Banshee\\ `banshee --query-volume`")
-        --, ((controlMask .|. shiftMask, xK_space),  
-        --    spawn "banshee --toggle-playing")
-        --, ((controlMask .|. shiftMask, xK_Return), spawn "banshee --stop")
-        --, ((controlMask .|. shiftMask, xK_m),      spawn "banshee --show")
-        ]
+  xmobarProc <- spawnPipe "/usr/bin/xmobar $HOME/.xmobarrc"
+  xmonad $ (myConfig xmobarProc) `additionalKeysP` 
+    [ 
+  --  Power-state keys
+      ("M-S-l", spawn "xscreensaver-command -lock")
+    , ("M-S-z", spawn "gksudo -g --message 'Suspend?' pm-suspend")
+    , ("M-S-e", spawn "gksudo -g --message 'Shut down?' poweroff")
+    , ("M-S-r", spawn "gksudo -g --message 'Reboot?' reboot")
+    , ("M-S-y", spawn "gksudo -g --message 'Hibernate?' pm-hibernate")
+
+  --  Utility keys
+    , ("M-<F12>", scratchpadSpawnActionCustom "gnome-terminal --name scratchpad")
+    --, ((controlMask, xK_Print), 
+    --    spawn "sleep 0.2; scrot -s -e \"mv \\$f ${SCREENSHOTS_DIR}\"")
+    --, ((0, xK_Print), spawn "scrot -e \"mv \\$f ${SCREENSHOTS_DIR}\"")
+  ----  Banshee keys
+    --, ((controlMask .|. shiftMask, xK_Right),  
+    --    spawn "banshee --next")
+    --, ((controlMask .|. shiftMask, xK_Left),   
+    --    spawn "banshee --restart-or-previous")
+    --            "notify-send Banshee\\ `banshee --query-volume`")
+    --, ((controlMask .|. shiftMask, xK_Down),   
+    --    spawn $ "banshee --set-volume=-10;"++
+    --            "notify-send Banshee\\ `banshee --query-volume`")
+    --, ((controlMask .|. shiftMask, xK_space),  
+    --    spawn "banshee --toggle-playing")
+    --, ((controlMask .|. shiftMask, xK_Return), spawn "banshee --stop")
+    --, ((controlMask .|. shiftMask, xK_m),      spawn "banshee --show")
+    ]
 
 myConfig xmobarProc = defaultConfig
         { manageHook = myManageHook <+> manageHook defaultConfig
