3e653194e8cec8397e372916c75a8d520fb26515
Daniel Asher Resnick <danwizard208@gmail.com>
2016-07-31 20:14:45 -0500

    Refactor xmonad.hs

--- home-links/.xmonad/xmonad.hs
+++ home-links/.xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+-- Imports {{{
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -15,46 +16,35 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.LayoutHints
 import System.IO
 import Data.Ratio ((%))
+-- }}}
 
 mainModMask = mod4Mask
 
 main = do
   xmobarProc <- spawnPipe "/usr/bin/xmobar $HOME/.xmobarrc"
   xmonad $ (myConfig xmobarProc) `additionalKeysP`
+  -- Keybindings {{{
     [
-  --  Power-state keys
+    --  Power-state keys {{{
       ("M-S-l", spawn "xscreensaver-command -lock")
     , ("M-S-z", spawn "systemctl suspend")
     -- , ("M-S-y", spawn "sudo pm-hibernate")
-    , ("M-S-e", spawn "gksudo -g --message 'Shut down?' poweroff")
-    , ("M-S-r", spawn "gksudo -g --message 'Reboot?' reboot")
-
-  --  Utility keys
+    , ("M-S-e", spawn "poweroff")
+    , ("M-S-r", spawn "reboot")
+    -- }}}
+    --  Utility keys {{{
+    , ("M-v", spawn $ myTerminal ++ " --exec='nvim'")
     , ("M-<F12>", namedScratchpadAction myScratchPads "terminal")
+    , ("M-d", namedScratchpadAction myScratchPads "todo")
     , ("M-b", sendMessage ToggleStruts)
+    , ("M-n", spawn "chromium --new-window")
     , ("M-p", spawn "dmenu_run -fn \
             \'DroidSansMonoForPowerline Nerd Font 16' -nf 'green' -sf 'green'")
+    -- }}}
     ]
+  -- }}}
 
-myTerminal = "termite"
-
-myWorkspaces =
-    (map show [1..6]) ++
-    ["7","8-Pidgin","9"]
-
-myScratchPads = [
-    NS "terminal" spawnTerm findTerm manageTerm
-    ]
-    where
-        spawnTerm = myTerminal ++ " --role=scratchpad"
-        findTerm = stringProperty "WM_WINDOW_ROLE" =? "scratchpad"
-        manageTerm = customFloating $ W.RationalRect l t w h
-            where           --All below are fractions of screen size
-                h = 0.4     --terminal height
-                w = 1       --terminal width
-                t = 0       --distance from top edge
-                l = 1 - w   --distance from left edge
-
+-- Base Config {{{
 myConfig xmobarProc = defaultConfig
         { workspaces = myWorkspaces
         , terminal = myTerminal
@@ -62,13 +52,20 @@ myConfig xmobarProc = defaultConfig
         , handleEventHook = myHandleEventHook <+> handleEventHook defaultConfig
         , layoutHook = myLayoutHook
         , modMask = mod4Mask
-        , logHook = dynamicLogWithPP xmobarPP
-                       { ppOutput = hPutStrLn xmobarProc
-                       , ppTitle = xmobarColor "green" "" . shorten 50
-                       }
+        , logHook = myLogHook xmobarProc
         , focusFollowsMouse = False
         }
+-- }}}
 
+myTerminal = "termite"
+
+-- Workspaces {{{
+myWorkspaces =
+    (map show [1..6]) ++
+    ["7","8-Pidgin","9"]
+-- }}}
+
+-- Layouts {{{
 myLayoutHook = layoutHintsToCenter $ avoidStruts $
     onWorkspace "7" skypeLayout $
     onWorkspace "8-Pidgin" imLayout $
@@ -99,19 +96,56 @@ myTallLayout = Tall nmaster delta ratio
         delta   = 2/100
         -- Default proportion of screen occupied by master pane
         ratio   = 1/2
+--}}}
 
-myManageHook = composeAll
-    [
+-- Hooks {{{
+myManageHook = composeAll [
       className =? "Xmessage" --> doFloat
     , resource =? "skype"    --> doShift "7"  -- Force to Skype workspace.
     , manageDocks
     , manageScratchPad
-    ]
+  ]
 
 myHandleEventHook = composeAll
     [
       hintsEventHook
     ]
 
+myLogHook xmobarProc =
+  dynamicLogWithPP xmobarPP {
+    ppOutput = hPutStrLn xmobarProc
+  , ppTitle = xmobarColor "green" "" . shorten 50
+  }
+-- }}}
+
+-- Scratchpads {{{
 manageScratchPad :: ManageHook
 manageScratchPad = namedScratchpadManageHook myScratchPads
+
+myScratchPads = [
+      NS "terminal" spawnTerm findTerm manageTerm
+    , NS "todo" spawnTodo findTodo manageTodo
+    ]
+    where
+        spawnTerm = myTerminal ++ " --role=scratchpad"
+        spawnTodo = myTerminal ++
+          " --directory=" ++ myTodirRoot ++
+          " --exec='nvim .' --role=todo"
+        findTerm = stringProperty "WM_WINDOW_ROLE" =? "scratchpad"
+        findTodo = stringProperty "WM_WINDOW_ROLE" =? "todo"
+        manageTerm = customFloating $ W.RationalRect l t w h
+            where           --All below are fractions of screen size
+                h = 0.5     --terminal height
+                w = 1       --terminal width
+                t = 0       --distance from top edge
+                l = 1 - w   --distance from left edge
+        manageTodo = customFloating $ W.RationalRect l t w h
+            where
+                h = 0.97
+                w = 0.5
+                t = 1 - h
+                l = 0
+
+myTodirRoot = "$HOME/todir"
+-- }}}
+-- vim:foldmethod=marker
